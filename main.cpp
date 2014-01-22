abeb46b2a02a9c1550638bb519a05f4363650a78
szymekh6 <szymekh6@vp.pl>
2014-01-22 16:47:01 +0100

    w mainie funkcja wyswietlajaca zawartosc tablicy

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
+#include<iostream>
+#include"gen.h"
 int main()
 {
-	
+	std::cout << "10 liczb:" << std::endl;
+	int * tmp = gen();
+	for(int i=0; i<10; i++)
+	{
+		std::cout<<tmp[i] << std::endl;
+	}
 	return 0;
 }
