62a120a2e8db7a005c56f48f553590c3583240c2
szymekh6 <szymekh6@vp.pl>
2014-01-22 17:37:53 +0100

    poprawiony main, dzialajacy

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include<iostream>
-#include"gen.h"
-#include"min.h"
 #include"utils.h"
 int main()
 {
 	int *tab = gener();
-	utils();
+	utils(tab);
 	int a = min(tab);
 	std::cout << "minimum to " << a << std::endl;
 
+	int b = max(tab);
+	std::cout << "maskimum to " << b << std::endl;
+	
+	int s = sum(tab);
+		std::cout << "suma to " << s << std::endl;
+	
 	return 0;
 }
--- utils.h
+++ utils.h
@@ -23,7 +23,7 @@ int max(int * tab)
 }
 int* gener()
 {
-int tab[10];
+int* tab = new int[10];
 for(int i=0;i<10;++i)
 {
 tab[i]=i;
